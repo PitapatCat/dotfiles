ceb8f133ba463290554279ea9796105341017210
PitapatCat <kevwjin@gmail.com>
2021-04-10 09:55:22 -0700

    Add global relative line numbers

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -13,38 +13,29 @@
 (define-key global-map "\C-ca" 'org-agenda)
 (setq org-log-done t)
 
-; remove auto indentation
-; (setq org-adapt-indentation nil)
-
 ; list behavior like in Google Docs
 (add-hook 'org-mode-hook (lambda () (org-autolist-mode)))
 
 ; word wrap
-(add-hook 'org-mode-hook #'(lambda ()
+(add-hook 'org-mode-hook #'(lambda () (visual-line-mode) (org-indent-mode)))
 
-                             ;; make the lines in the buffer wrap around the edges of the screen.
-                             
-                             ;; to press C-c q  or fill-paragraph ever again!
-                             (visual-line-mode)
-                             (org-indent-mode)))
-
-; expands snippets
+; expands snippets (e.g. code block)
 (require 'org-tempo)
 
 (define-key text-mode-map (kbd "TAB") 'self-insert-command);
-(custom-set-variables
+;; (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages
-   '(org-agenda-property cloud-to-butt-erc cloud-theme twilight-bright-theme base16-theme evil-org org-autolist evil)))
-(custom-set-faces
+ ;; '(package-selected-packages
+   ;; '(org-agenda-property cloud-to-butt-erc cloud-theme twilight-bright-theme base16-theme evil-org org-autolist evil)))
+;; (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ ;; )
 
 ; evil-org: provides better vim bindings
 (use-package evil-org
@@ -76,3 +67,23 @@
 
 ;; remove bell sound
 (setq visible-bell t) 
+
+; relative line numbers
+(menu-bar-display-line-numbers-mode 'relative)
+(add-hook 'foo-mode-hook #'display-line-numbers-mode)
+(global-display-line-numbers-mode 1)
+(setq display-line-numbers-type 'relative)
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   '(linum-relative use-package twilight-bright-theme org-autolist org-agenda-property nlinum-relative evil-org cloud-to-butt-erc cloud-theme base16-theme apropospriate-theme)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
